18a72b5e90354174e3a58309e4e9fbe42d70f6e4
Artem6evelev <artem6evelev92@gmail.com>
2022-06-12 08:21:58 +0300

    getting value from inputs, and simple UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
+import React, { useState } from "react";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [word, setWord] = useState("");
+  const [page, setPage] = useState(0);
+
+  const displayInfo = () => {
+    console.log(word + page);
+  };
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label>Word:</label>
+        <input type="text" onChange={(event) => setWord(event.target.value)} />
+        <label>Page:</label>
+        <input
+          type="number"
+          onChange={(event) => setPage(event.target.value)}
+        />
+        <button onClick={displayInfo}>Add word</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
